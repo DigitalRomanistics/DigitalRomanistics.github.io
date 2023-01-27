6553966de544e018845d276e1a06cc709a7dafe3
helenasabel <helena.b.sabel@gmail.com>
2023-01-27 15:13:28 +0100

    added one example per category

--- assets/js/show.js
+++ assets/js/show.js
@@ -1,5 +1,5 @@
 $(function() {
-    $('.title').click(function() {
+    $('.heading').click(function() {
         $(this).toggleClass('active').next().children('.content').toggleClass('show');
     });
 });
